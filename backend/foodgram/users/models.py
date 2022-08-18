cab44585404d1bf78e6f44664b47d4dab7d1b091
Ali Bogatyrev <marksheyder06@gmail.com>
2022-08-18 21:21:56 +0300

    added user's model

--- backend/foodgram/users/models.py
+++ backend/foodgram/users/models.py
@@ -1,3 +1,62 @@
+from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.validators import UnicodeUsernameValidator
 from django.db import models
 
-# Create your models here.
+USER = 'user'
+ADMIN = 'admin'
+
+
+class User(AbstractUser):
+    """Модель пользователей."""
+    ROLES = (
+        (USER, USER),
+        (ADMIN, ADMIN),
+    )
+    username_validator = UnicodeUsernameValidator()
+
+    username = models.CharField(
+        max_length=150,
+        unique=True,
+        validators=[username_validator],
+        verbose_name='Имя пользователя'
+    )
+    first_name = models.CharField(
+        max_length=150,
+        blank=True,
+        verbose_name='Имя',
+    )
+    last_name = models.CharField(
+        max_length=150,
+        blank=True,
+        verbose_name='Фамилия',
+    )
+    email = models.EmailField(
+        max_length=254,
+        unique=True,
+        verbose_name='email',
+    )
+    role = models.CharField(
+        choices=ROLES,
+        max_length=25,
+        default=USER,
+        verbose_name='Роль пользователя'
+    )
+
+    REQUIRED_FIELDS = ['email']
+    USERNAME_FIELD = 'email'
+
+    class Meta:
+        ordering = (id,)
+        verbose_name = 'Пользователь'
+        verbose_name_plural = 'Пользователи'
+
+    def __str__(self):
+        return str(self.username)
+
+    @property
+    def is_admin(self):
+        return self.role == ADMIN or self.is_superuser
+
+    @property
+    def is_user(self):
+        return self.role == USER
