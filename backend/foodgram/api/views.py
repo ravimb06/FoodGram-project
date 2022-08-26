3d1b2642d95d2f43996c160cc75feb6ee97e7541
Ali Bogatyrev <marksheyder06@gmail.com>
2022-08-27 01:50:53 +0300

    added api UsersViewSet

--- backend/foodgram/api/views.py
+++ backend/foodgram/api/views.py
@@ -1,3 +1,50 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404
+from djoser.views import UserViewSet
+from rest_framework import status
+from rest_framework.decorators import action
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
 
-# Create your views here.
+from .pagination import CustomPagination
+from .serializers import (FollowListSerializer, FollowSerializer)
+from users.models import Follow, User
+
+
+class UsersViewSet(UserViewSet):
+    pagination_class = CustomPagination
+
+    @action(['get'], detail=False, permission_classes=[IsAuthenticated])
+    def me(self, request, *args, **kwargs):
+        self.get_object = self.get_instance
+        return self.retrieve(request, *args, **kwargs)
+
+    @action(methods=['get'], detail=False)
+    def subscriptions(self, request):
+        subscriptions_list = self.paginate_queryset(
+            User.objects.filter(following__user=request.user)
+        )
+        serializer = FollowListSerializer(
+            subscriptions_list, many=True, context={'request': request}
+        )
+        return self.get_paginated_response(serializer.data)
+
+    @action(methods=['post', 'delete'], detail=True)
+    def subscribe(self, request, id):
+        if request.method != 'POST':
+            subscription = get_object_or_404(
+                Follow,
+                author=get_object_or_404(User, id=id),
+                user=request.user
+            )
+            self.perform_destroy(subscription)
+            return Response(status=status.HTTP_204_NO_CONTENT)
+        serializer = FollowSerializer(
+            data={
+                'user': request.user.id,
+                'author': get_object_or_404(User, id=id).id
+            },
+            context={'request': request}
+        )
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
