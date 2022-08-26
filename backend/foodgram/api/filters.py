857715439d281202f8b7da2102eaeaf6d162cb68
Ali Bogatyrev <marksheyder06@gmail.com>
2022-08-26 22:04:41 +0300

    created api.filters

---
+++ backend/foodgram/api/filters.py
@@ -0,0 +1,30 @@
+from django_filters.rest_framework import FilterSet
+from django_filters.rest_framework.filters import (AllValueMultipleFilter,
+                                                   BooleanFilter)
+from rest_framework.filters import SearchFilter
+
+from recipes.models import Recipe
+
+
+class RecipeFilterSet(FilterSet):
+    tags = AllValueMultipleFilter(field_name='tags__slug')
+    is_favorited = BooleanFilter(method='get_is_favorited')
+    is_in_shopping_cart = BooleanFilter(method='get_is_in_shopping_cart')
+
+    class Meta:
+        model = Recipe
+        fields = ('author', 'is_favorited', 'tags', 'is_in_shopping_cart')
+
+    def get_is_favorited(self, queryset, name, value):
+        if self.request.user.is_authenticated and value:
+            return queryset.filter(in_favorite__user==self.request.user)
+        return queryset
+
+    def get_is_in_shopping_cart(self, queryset, name, value):
+        if self.request.user.is_authenticated and value:
+            return queryset.filter(cart__user=self.request.user)
+        return queryset.all()
+
+
+class IngredientSearchFilter(SearchFilter):
+    search_param = 'name'
