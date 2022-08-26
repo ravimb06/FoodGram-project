a97c67bf6ff7e4142deeda374a914eb9135b4691
Ali Bogatyrev <marksheyder06@gmail.com>
2022-08-26 17:46:04 +0300

    created CartSerializer and FavoriteSerializer

--- backend/foodgram/api/serializers.py
+++ backend/foodgram/api/serializers.py
@@ -272,3 +272,48 @@ class CreateRecipeSerializer(ModelSerializer):
                 'request': self.context.get('request')
             }
         ).data
+
+
+class CartSerializer(ModelSerializer):
+    class Meta:
+        fields = ['recipe', 'user']
+        model = Cart
+
+    def validate(self, data):
+        request = self.context.get('request')
+        recipe = data['recipe']
+        if Cart.objects.filter(
+            user=request.user, recipe=recipe
+        ).exists():
+            raise ValidationError({
+                'errors': 'Этот рецепт уже в корзине.'
+            })
+        return data
+
+    def to_representation(self, instance):
+        request = self.context.get('request')
+        context = {'request': request}
+        return RecipeShortInfo(instance.recipe, context=context).data
+
+
+class FavoriteSerializer(ModelSerializer):
+    class Meta:
+        model = Favorite
+        fields = ('user', 'recipe')
+
+    def validate(self, data):
+        request = self.context.get('request')
+        if not request or request.user.is_anonymous:
+            return False
+        recipe = data['recipe']
+        if Favorite.objects.filter(user=request.user, recipe=recipe).exists():
+            raise ValidationError({
+                'errors': 'Уже есть в избранном.'
+            })
+        return data
+
+    def to_representation(self, instance):
+        request = self.context.get('request')
+        context = {'request': request}
+        return RecipeShortInfo(
+            instance.recipe, context=context).data
