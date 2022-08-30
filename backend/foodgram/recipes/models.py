c7d066e1ebd687e217f650b0eb4315e274a0d21d
Ali Bogatyrev <marksheyder06@gmail.com>
2022-08-30 22:14:53 +0300

    correction of the code for the requirements of pep8

--- backend/foodgram/recipes/models.py
+++ backend/foodgram/recipes/models.py
@@ -9,9 +9,9 @@ User = get_user_model()
 class Ingredient(models.Model):
     """Модель ингредиента."""
     name = models.CharField(
-       max_length=200,
-       verbose_name='Название ингредиента',
-       db_index=True
+        max_length=200,
+        verbose_name='Название ингредиента',
+        db_index=True
     )
     measurement_unit = models.CharField(
         max_length=200,
